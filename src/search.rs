4d433b151e45b3f3f000801508569d6ecf98c1f7
Phil-Brooks <phil@brookshome.co.uk>
2025-07-06 10:21:08 +0100

    first version of depth

--- src/search.rs
+++ src/search.rs
@@ -1,49 +1,43 @@
-use crate::evaluation::{self, EvalTable};
 use cozy_chess::*;
-use std::str::FromStr;
+use crate::evaluation;
 
 pub struct AlphaBetaSearcher {
-    root_best_move: Move,
     root_score: i32,
     nodes: u64,
-    evalt:EvalTable 
+    best_move:String,
+    curr_pv :String,
 }
 impl AlphaBetaSearcher {
     pub fn new() -> Self {
         AlphaBetaSearcher {
-            root_best_move: Move::from_str("a1a4").unwrap(),
             root_score: 0,
             nodes: 0,
-            evalt: EvalTable::default(),
+            best_move: "".to_string(),
+            curr_pv: "".to_string(),
         }
     }
     pub fn get_best_move(&mut self, board: &Board, depth: i32) -> String {
         let mut current_depth: i32 = 1;
-        let final_move: String;
         self.nodes = 0;
-        self.root_best_move = Move::from_str("a1a4").unwrap();
-
+    
         let alpha: i32 = -99999999;
         let beta: i32 = 99999999;
 
         while current_depth <= depth {
-            let score: i32 = self.alpha_beta(board, alpha,  beta, current_depth);
-            println!("best move {} at depth {} score cp {}", self.root_best_move, current_depth, score);
+            self.best_move = "".to_string();
+            self.root_score = -99999999;
+            let score: i32 = self.alpha_beta(board, alpha,  beta, current_depth,0);
+            println!("info depth {} time 0 nodes 0 score cp {} pv {}", current_depth, score, self.best_move);
             current_depth += 1;
         }
-        final_move = self.root_best_move.clone().to_string();
+        let final_move = self.best_move.clone();
 
-        println!(
-            "info depth {} score cp {}",
-            current_depth - 1,
-            self.root_score
-        );
         return final_move;
     }
-    fn alpha_beta(&mut self, board: &Board, mut alpha: i32, beta: i32, depthleft: i32) -> i32 {
+    fn alpha_beta(&mut self, board: &Board, mut alpha: i32, beta: i32, depthleft: i32,ply: u32) -> i32 {
         if depthleft == 0 {
-            //return evaluation::eval(board, &mut self.evalt);
-            return evaluation::eval_from_scratch(board);
+            let eval = evaluation::eval_from_scratch(board);
+            return eval;
         }
         let mut best_value = -99999999;
         //generate all moves and store them in a vector
@@ -56,11 +50,15 @@ impl AlphaBetaSearcher {
         });
         for mov in moves.iter() {
             let mut new_board = board.clone();
+            let old_pv = self.curr_pv.clone();
             new_board.play_unchecked(*mov);
-            let score = -self.alpha_beta(&new_board, -beta, -alpha, depthleft - 1);
+            self.curr_pv = (format!("{} {}", self.curr_pv, mov.to_string())).trim().to_string();
+            let score = -self.alpha_beta(&new_board, -beta, -alpha, depthleft - 1,ply+1);
             if score > best_value {
-                self.root_best_move = *mov; // update the best move
-                self.root_score = score; // update the root score
+                if ply == 0 {
+                    self.root_score = score; 
+                    self.best_move = self.curr_pv.clone(); 
+                }
                 best_value = score;
                 if score > alpha {
                     alpha = score; // alpha acts like max in MiniMax
@@ -69,6 +67,7 @@ impl AlphaBetaSearcher {
             if score >= beta {
                 return best_value;
             } //  fail soft beta-cutoff, existing the loop here is also fine
+            self.curr_pv = old_pv; // restore the previous principal variation
         }
         return best_value;
     }
